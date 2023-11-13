ccd54c691e9be31855c2905ae425fd5f4632e87a
SosukeAizen833 <Spandan833@gmai.com>
2023-11-13 23:09:20 +0530

    Created One to one unidirectional relationship between Ingredient and Unit of Measure

--- src/main/java/com/springframework/springrecipeapp/domain/Ingredient.java
+++ src/main/java/com/springframework/springrecipeapp/domain/Ingredient.java
@@ -12,6 +12,17 @@ public class Ingredient {
     private String description;
     private BigDecimal amount;
 
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure unitOfMeasure;
+
+    public UnitOfMeasure getUnitOfMeasure() {
+        return unitOfMeasure;
+    }
+
+    public void setUnitOfMeasure(UnitOfMeasure unitOfMeasure) {
+        this.unitOfMeasure = unitOfMeasure;
+    }
+
     public Long getId() {
         return id;
     }
